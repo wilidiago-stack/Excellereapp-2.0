91d548ff545293c391b12f1abbdf977922196d4a
Andres Diago <wilidiago@gmail.com>
2026-02-16 21:43:01 +0000

    Error: Firebase Storage has not been set up on project 'studio-354994930

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,6 @@
 export const firebaseConfig = {
   "projectId": "studio-2845988015-3b127",
   "appId": "1:1014968267072:web:3b1e94fe8c1fad80fccaa5",
-  "storageBucket": "studio-2845988015-3b127.appspot.com",
   "apiKey": "AIzaSyBcLoQ78sf1Obo8B5hihy5cUPpn-hQhfqM",
   "authDomain": "studio-2845988015-3b127.firebaseapp.com",
   "measurementId": "G-CEDVZ5BXXC",
