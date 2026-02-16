2b2fb805b22a30f41f3034c6f4c2f1d1da6cade2
Andres Diago <wilidiago@gmail.com>
2026-02-16 21:51:12 +0000

    bueno, estaba consultando firebase consola, y no veo ninguna base de dat

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-2845988015-3b127",
+  "projectId": "studio-3549949301-83f54",
   "appId": "1:1014968267072:web:3b1e94fe8c1fad80fccaa5",
   "apiKey": "AIzaSyBcLoQ78sf1Obo8B5hihy5cUPpn-hQhfqM",
-  "authDomain": "studio-2845988015-3b127.firebaseapp.com",
+  "authDomain": "studio-3549949301-83f54.firebaseapp.com",
+  "storageBucket": "studio-3549949301-83f54.appspot.com",
   "measurementId": "G-CEDVZ5BXXC",
   "messagingSenderId": "1014968267072"
 };
